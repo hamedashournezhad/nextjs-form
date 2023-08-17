e34f9455c1df6c9125d544eecae53f864458bcaa
rebanso <rebansochart@gmail.com>
2023-08-17 12:31:52 +0330

    v-alpha-1.0.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,38 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./assets/App.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className={styles.app}>
+      <div className={styles.app_animate}></div>
+      <form className={styles.form}>
+        <div className={styles.form_animate}></div>
+        <label htmlFor="firstname">Firstname:</label>
+        <input
+          type="text"
+          placeholder="What's your first name?"
+          autoComplete="off"
+          id="firstname"
+          pattern="[a-zA-Z]"
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <label htmlFor="lastname">Lastname:</label>
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="What's your first name?"
+          id="lastname"
+          pattern="[a-zA-Z]"
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          autoComplete="current-password"
+          placeholder="choose a password.."
+          id="password"
+        />
+        <button type="submit" id="FormSubmitHandler">
+          Sign-up
+        </button>
+      </form>
     </main>
-  )
+  );
 }
